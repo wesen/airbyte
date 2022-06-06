59896601806a96c19927ddc1acd6c4c6c2b4a9e0
Edmundo Ruiz Ghanem <edmundo@airbyte.io>
2022-06-06 12:59:20 -0400

    Add more auto-fixable rules and fix

--- airbyte-webapp/src/packages/cloud/views/auth/SignupPage/SignupPage.tsx
+++ airbyte-webapp/src/packages/cloud/views/auth/SignupPage/SignupPage.tsx
@@ -37,7 +37,7 @@ const SignupPageValidationSchema = yup.object().shape({
 });
 
 const SignupPage: React.FC = () => {
-  const formatMessage = useIntl().formatMessage;
+  const { formatMessage } = useIntl();
   const config = useConfig();
 
   const { signUp } = useAuthService();
